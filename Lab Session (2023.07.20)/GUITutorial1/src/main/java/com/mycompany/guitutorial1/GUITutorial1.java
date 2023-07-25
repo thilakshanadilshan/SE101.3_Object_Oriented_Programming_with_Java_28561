0fb6730a70970486968783a41ab32545502b4be1
Thilakshana Dilshan Randeniya <116573168+thilakshanadilshan@users.noreply.github.com>
2023-07-25 23:05:41 +0530

    GUI

---
+++ Lab Session (2023.07.20)/GUITutorial1/src/main/java/com/mycompany/guitutorial1/GUITutorial1.java
@@ -0,0 +1,9 @@
+package com.mycompany.guitutorial1;
+public class GUITutorial1
+{
+    public static void main(String[] args) 
+    {
+        GUITiuto cc = new GUITiuto();
+        cc.show();
+    }
+}
---
+++ Lab Session (2023.07.20)/GUITutorial2/src/main/java/com/mycompany/guitutorial2/GUITutorial2.java
@@ -0,0 +1,9 @@
+package com.mycompany.guitutorial2;
+public class GUITutorial2 
+{
+    public static void main(String[] args)
+    {
+        ClickCounter cc = new ClickCounter();
+        cc.show();
+    }
+}
